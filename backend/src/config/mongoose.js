33c17b33c52ff5506868723869cfb927070f1682
Elin Östman <elin.ostman@hotmail.com>
2024-04-23 21:07:12 +0200

    env fil check, den funkar

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
-const databaseURI =
-  process.env.MONGOOSE_LIVE_URI ||
-  "mongodb+srv://eomolle:mxFo7XWrsQ9EtCPR@elinsblog.wfkbky3.mongodb.net/?retryWrites=true&w=majority&appName=Elins_blog";
+const databaseURI = process.env.MONGOOSE_LIVE_URI;
+// "mongodb+srv://eomolle:mxFo7XWrsQ9EtCPR@elinsblog.wfkbky3.mongodb.net/?retryWrites=true&w=majority&appName=Elins_blog";
 
 // "mongodb+srv://eomolle:mxFo7XWrsQ9EtCPR@elins_blog.tz53jqs.mongodb.net/?retryWrites=true&w=majority&appName=Elins_blog";
 
