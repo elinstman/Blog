5e459060cbfabc2c4a96802886be95ab53f199ce
Elin Östman <elin.ostman@hotmail.com>
2024-04-23 21:08:43 +0200

    deleted old uri link

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
 const databaseURI = process.env.MONGOOSE_LIVE_URI;
-// "mongodb+srv://eomolle:mxFo7XWrsQ9EtCPR@elinsblog.wfkbky3.mongodb.net/?retryWrites=true&w=majority&appName=Elins_blog";
-
-// "mongodb+srv://eomolle:mxFo7XWrsQ9EtCPR@elins_blog.tz53jqs.mongodb.net/?retryWrites=true&w=majority&appName=Elins_blog";
 
 function connectToMongoose() {
   mongoose
