8a2f5349fb0e5da6e999718e7778e3dcfe81346e
Elin Östman <elin.ostman@hotmail.com>
2024-04-30 18:18:50 +0200

    skapa loginUser och RegisterUser komponenter, som får visas ut villkorligt på loginPage. Villkor ej skapade ännu

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -1,29 +1,13 @@
 import Header from "../Components/Header"
+import UserLogin from "../Components/Login"
+// import RegisterUser from "../Components/RegisterUser"
 
 const LoginPage = () => {
     return (
         <>
         <Header />
-        <form>
-            <h2>Välkommen tillbaka!</h2>
-            <div className="login-div"> 
-            <div className="form-floating">
-                <input type="email" id="loginEmail" className="form-control"/>
-                <label htmlFor="loginEmail">Mailadress</label>
-            </div>
-
-            <div className="form-floating">
-            <input type="password" id="loginPassword" className="form-control"/>
-                <label htmlFor="loginPassword">Lösenord</label>
-            </div>
-            <div className="button-login-container"> 
-            <button class="btn w-100 py-2 button-login" type="submit">Logga in</button>
-            </div>
-            </div>
-
-            
-
-        </form>
+        <UserLogin />
+        {/* <RegisterUser /> */}
         
         </>
     )
