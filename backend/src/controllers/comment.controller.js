c310965c4ecc809434455d89d76f03789b9f2ce3
Elin Östman <elin.ostman@hotmail.com>
2024-05-08 23:55:27 +0200

    create comment, ok. get comments for specifikt postId with params, ok

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -22,6 +22,28 @@ async function createComment(req, res) {
   }
 }
 
+async function getComments(req, res) {
+  try {
+    const postId = req.params.postId;
+    const comments = await Comment.find({ post: postId }).populate({
+      path: "author",
+      select: "userName id",
+    });
+
+    if (!comments || comments.length === 0) {
+      return res.status(404).json({
+        message: "Inga kommentarer hittades för det angivna inlägget",
+      });
+    }
+
+    res.status(200).json(comments);
+  } catch (error) {
+    console.log("error getting comment", error);
+    res.status(500).json({ message: "Serverfel vid hämtning av kommentarer" });
+  }
+}
+
 module.exports = {
   createComment,
+  getComments,
 };
